dd0542abc829fb17fd716a9d8fd77476483ca66e
ogorkun <ogorkun@adobe.com>
2021-01-11 13:21:38 -0600

    client update

--- client/src/main/java/com/magento/grpctest/client/ServerTest.java
+++ client/src/main/java/com/magento/grpctest/client/ServerTest.java
@@ -1,5 +1,7 @@
 package com.magento.grpctest.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +13,9 @@ import javax.validation.constraints.NotNull;
 import java.time.Duration;
 
 @RestController
-@RequestMapping("/invoke-generate")
-public class GenerateTest {
-    public static class TestResponse {
+@RequestMapping("/")
+public class ServerTest {
+    public static class GenerateResponse {
         private Duration phpClientTime;
 
         private Duration phpAsyncClientTime;
@@ -22,7 +24,7 @@ public class GenerateTest {
 
         private Duration javaAsyncClientTime;
 
-        public TestResponse() { }
+        public GenerateResponse() { }
 
         public Duration getPhpClientTime() {
             return phpClientTime;
@@ -57,6 +59,46 @@ public class GenerateTest {
         }
     }
 
+    public static class ClearResponse {
+        private Boolean javaCleared;
+
+        private Boolean phpCleared;
+
+        public ClearResponse(boolean javaCleared, boolean phpCleared) {
+            this.javaCleared = javaCleared;
+            this.phpCleared = phpCleared;
+        }
+
+        public Boolean getJavaCleared() {
+            return javaCleared;
+        }
+
+        public Boolean getPhpCleared() {
+            return phpCleared;
+        }
+    }
+
+    public static class ReadResponse {
+        private Duration phpReadTime;
+
+        private Duration javaReadTime;
+
+        public ReadResponse() {}
+
+        public ReadResponse(Duration phpReadTime, Duration javaReadTime) {
+            this.phpReadTime = phpReadTime;
+            this.javaReadTime = javaReadTime;
+        }
+
+        public Duration getPhpReadTime() {
+            return phpReadTime;
+        }
+
+        public Duration getJavaReadTime() {
+            return javaReadTime;
+        }
+    }
+
     public enum Client {
         PHP,
         JAVA
@@ -66,19 +108,21 @@ public class GenerateTest {
 
     private final GrpcClient javaClient;
 
-    public GenerateTest(@Autowired GrpcClient phpClient, @Autowired GrpcClient javaClient) {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public ServerTest(@Autowired GrpcClient phpClient, @Autowired GrpcClient javaClient) {
         this.phpClient = phpClient;
         this.javaClient = javaClient;
     }
 
-    @GetMapping
-    public ResponseEntity<TestResponse> generate(@Valid @NotNull @Min(1) @RequestParam("count") Integer count,
+    @GetMapping(value = "invoke-generate")
+    public ResponseEntity<GenerateResponse> generate(@Valid @NotNull @Min(1) @RequestParam("count") Integer count,
                                                  @Valid @NotNull @Min(1) @RequestParam("connections") Integer connections,
                                                  @Valid @RequestParam("client") Client client) {
         if (client == null) {
             client = Client.PHP;
         }
-        var resp = new TestResponse();
+        var resp = new GenerateResponse();
         if (client == Client.PHP) {
             resp.setPhpClientTime(phpClient.callGenerate(false, count, connections));
             resp.setPhpAsyncClientTime(phpClient.callGenerate(true, count, connections));
@@ -92,5 +136,35 @@ public class GenerateTest {
         }
 
         return new ResponseEntity<>(resp, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "clear")
+    public ResponseEntity<ClearResponse> clear() {
+        boolean javaCleared = true;
+        boolean phpCleared = true;
+        try {
+            javaClient.clear();
+        } catch (Throwable ex) {
+            javaCleared = false;
+            logger.error(String.format("Error while clearing JAVA server: %s", ex.getMessage()));
+        }
+        try {
+            phpClient.clear();
+        } catch (Throwable ex) {
+            phpCleared = false;
+            logger.error(String.format("Error while clearing PHP server: %s", ex.getMessage()));
+        }
+
+        return new ResponseEntity<>(new ClearResponse(javaCleared, phpCleared), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "invoke-read")
+    public ResponseEntity<ReadResponse> read(@Valid @NotNull @Min(1) @RequestParam("number") Integer number,
+                                             @Valid @NotNull @Min(1) @RequestParam("connections") Integer connections) {
+
+        return new ResponseEntity<>(
+                new ReadResponse(phpClient.callRead(number, connections), javaClient.callRead(number, connections)),
+                HttpStatus.OK
+        );
     }
 }
